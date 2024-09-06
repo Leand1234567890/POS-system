f6f5cdf439ba546c0d6f3004128bc1d6b7807dd4
Leand1234567890 <143790122+Leand1234567890@users.noreply.github.com>
2024-09-06 14:19:57 +0200

    Add files via upload

---
+++ POS-code/POS/src/main/java/com/mycompany/pos/POS.java
@@ -0,0 +1,17 @@
+
+package com.mycompany.pos;
+
+/**
+ *
+ * @author Leandro
+ * @author Aashiq
+ * @author Matthew
+ * @author Tshiamo
+ */
+public class POS {
+
+    public static void main(String[] args) {
+        System.out.println("Starting application");
+        
+    }
+}
