a7a2a19d9eaaf7e40f0c9fa136516f5f98307a92
Leand1234567890 <143790122+Leand1234567890@users.noreply.github.com>
2024-09-08 20:59:24 +0200

    Add files via upload

---
+++ posLogic/src/main/java/com/mycompany/poslogic/PosLogic.java
@@ -0,0 +1,18 @@
+
+package com.mycompany.poslogic;
+
+/**
+ *
+ * @author Leandro
+ */
+public class PosLogic {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        java.awt.EventQueue.invokeLater(new Runnable() {
+            public void run() {
+                new titleForm().setVisible(true);
+            }
+        });
+    }
+}
